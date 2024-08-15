8c61831641e792a98c54b3bf4f1664bde2c1eef9
tajtanvvir1 <tanvirah1223@gmail.com>
2024-08-15 23:58:34 +0600

    Bsic Setup

---
+++ src/Routes/router.jsx
@@ -0,0 +1,17 @@
+import { createBrowserRouter } from "react-router-dom";
+import Root from "../Layout/Root";
+
+const router = createBrowserRouter([
+   {
+      path: "/",
+      element: <Root></Root>,
+      children: [
+         {
+            path: "/",
+            element: 
+         }
+      ]
+   }
+])
+
+export default router;
