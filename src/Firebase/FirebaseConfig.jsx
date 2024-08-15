5e2fbcbf3d24db3221d3511af1cb831921100e43
tajtanvvir1 <tanvirah1223@gmail.com>
2024-08-16 02:40:35 +0600

    Firebase Added

---
+++ src/Firebase/FirebaseConfig.jsx
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDI-cE2xdQtpbboRIyTY61vBoss-H3Im1E",
+  authDomain: "taj-tech-mart.firebaseapp.com",
+  projectId: "taj-tech-mart",
+  storageBucket: "taj-tech-mart.appspot.com",
+  messagingSenderId: "923591402468",
+  appId: "1:923591402468:web:8452caa465b8b7d3d99df1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
