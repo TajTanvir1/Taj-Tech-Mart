fcf09dbc4dd177fa439523dcf1e306b07628ccaa
tajtanvvir1 <tanvirah1223@gmail.com>
2024-08-17 20:58:38 +0600

    Register page added

--- public/links.js
+++ public/links.js
@@ -2,7 +2,34 @@
 LOGO
 https://i.ibb.co/T1NnN4d/TTM-logo.png
 
+Pictures
 
+https://i.ibb.co/BrXPxg2/cam-1.png
+https://i.ibb.co/6Dcq34f/cpu-1.png
+https://i.ibb.co/XSwg0sr/cpu-2.png
+https://i.ibb.co/hMQNjXz/cpu-3.png
+https://i.ibb.co/drR5qZk/dr-2.png
+https://i.ibb.co/NLsD0nV/dr-3.png
+https://i.ibb.co/ngP7QWY/dr.png
+https://i.ibb.co/kJDXTdJ/Laptop-2.png
+https://i.ibb.co/DL5zwz6/Lp-3.png
+https://i.ibb.co/CvgqtXG/LP-4.png
+https://i.ibb.co/cNmqb35/lp-5.png
+https://i.ibb.co/pKLrFQB/lp-6.png
+https://i.ibb.co/9njrxL0/mb-1.jpg
+https://i.ibb.co/QvBXCS1/mb-1.png
+https://i.ibb.co/Y2b6RWP/mb-2.png
+https://i.ibb.co/HKNGtqd/mb-3.png
+https://i.ibb.co/gTqR9XT/mon-2.png
+https://i.ibb.co/1G9KR2F/moni.png
+https://i.ibb.co/8Kprx7C/pr-1.png
+https://i.ibb.co/ccKJVNf/SW-1.png
+https://i.ibb.co/bQ07nFL/sw-2.png
+https://i.ibb.co/VMM6W0G/sw-4.png
+https://i.ibb.co/FWJc8mC/sw-5.png
+https://i.ibb.co/jZw5R3J/sw3.png
+https://i.ibb.co/XXc9kPY/tab-1.png
+https://i.ibb.co/44tdd6p/Untitled-1.png
 
 
 
