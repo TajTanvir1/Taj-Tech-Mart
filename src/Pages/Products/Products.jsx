1f733cd1497df4d87add7ec26f19f4256439188a
tajtanvvir1 <tanvirah1223@gmail.com>
2024-08-20 02:59:26 +0600

    Filter by Brand Added

--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { BsSearch } from "react-icons/bs";
 import ProductsCard from "./ProductsCard";
+import { TbFilterQuestion } from "react-icons/tb";
 
 
 const Products = () => {
@@ -30,25 +31,57 @@ const Products = () => {
             Products
          </h2>
 
-         
+         {/* Search , Filters Funtions */}
 
-         {/* Search Functions */}
-         <div className="mx-auto flex justify-end">
-            <form onSubmit={handleSearch}>
-               <div className="flex justify-between p-1 overflow-hidden rounded-lg border-2 border-orange-300 mr-2 md:mr-4">
-                  <input
-                     className="md:px-5 px-2 py-2 text-gray-700 bg-white outline-none"
-                     type="text"
-                     onChange={(e) => setSearchText(e.target.value)}
-                     value={searchText}
-                     name="search"
-                     placeholder="Search Product by Title"
-                  />
-                  <button className="btn btn-accent text-lg">
-                     <BsSearch /> Search
-                  </button>
-               </div>
-            </form>
+         <div className="flex lg:flex-row flex-col-reverse justify-center lg:justify-around">
+            {/* Filter by Brand */}
+            <div className="dropdown md:ml-4">
+               <div tabIndex={0} role="button" className="btn m-1"><TbFilterQuestion/> Brand Name</div>
+               <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] p-2 shadow">
+               {
+               products.map((brand) => (<>
+               
+                  <li key={brand._id}><button>{brand.brandName}</button></li>
+               
+               </>))
+               }
+               </ul>
+            </div>
+
+            {/* Filter by Categories */}
+            <div className="dropdown md:ml-4">
+               <div tabIndex={0} role="button" className="btn m-1"><TbFilterQuestion/> Category Name</div>
+               <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] p-2 shadow">
+               {
+               products.map((category) => (<>
+               
+                  <li key={category._id}><button>{category.categoryName}</button></li>
+               
+               </>))
+               }
+               </ul>
+            </div>
+
+            
+
+            {/* Search Functions */}
+            <div className="mx-auto flex lg:flex-none justify-end">
+               <form onSubmit={handleSearch}>
+                  <div className="flex justify-between p-1 overflow-hidden rounded-lg border-2 border-orange-300 mr-2 md:mr-4">
+                     <input
+                        className="md:px-5 px-2 py-2 text-gray-700 bg-white outline-none"
+                        type="text"
+                        onChange={(e) => setSearchText(e.target.value)}
+                        value={searchText}
+                        name="search"
+                        placeholder="Search Product by Title"
+                     />
+                     <button className="btn btn-accent text-lg">
+                        <BsSearch /> Search
+                     </button>
+                  </div>
+               </form>
+            </div>
          </div>
 
 
